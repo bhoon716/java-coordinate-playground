da250d4cd1464708ceeaa37d8a318259f27c0d5e
bhoon <leafloveu@naver.com>
2025-01-20 19:15:38 +0900

    feat(coordinate:square) - 직사각형 조건 판별 로직 추가

--- src/main/java/coord2/domain/Square.java
+++ src/main/java/coord2/domain/Square.java
@@ -1,20 +1,28 @@
 package coord2.domain;
 
+import coord2.exception.IllegalSquareException;
+
 public class Square implements Shape {
 
     private final Point[] points;
 
-    private Square(Point[] points) {
+    public Square(Point[] points) {
         this.points = points;
     }
 
-    public static Square create(Point[] points){
+    public static Square create(Point[] points) {
         validate(points);
         return new Square(points);
     }
 
     private static void validate(Point[] points) {
-
+        if (points.length != 4) {
+            throw new IllegalSquareException();
+        }
+        if ((points[0].calcDistance(points[1]) != points[2].calcDistance(points[3]))
+                || (points[0].calcDistance(points[2]) != points[1].calcDistance(points[3]))) {
+            throw new IllegalSquareException();
+        }
     }
 
     @Override
