022930d94c7073ecd135ffed7f0187a04b53cdb5
bhoon <leafloveu@naver.com>
2025-01-21 00:45:18 +0900

    refactor(coordinate:square) - 입력 양식 validator 삭제

--- src/main/java/coord2/view/InputViewImpl.java
+++ src/main/java/coord2/view/InputViewImpl.java
@@ -2,8 +2,6 @@ package coord2.view;
 
 import java.util.Scanner;
 
-import static coord2.view.InputValidator.validateInput;
-
 public class InputViewImpl implements InputView {
 
     private final Scanner sc = new Scanner(System.in);
@@ -11,7 +9,6 @@ public class InputViewImpl implements InputView {
     @Override
     public String readPoints() {
         String input = sc.nextLine();
-        validateInput(input);
         return input;
     }
 }
