809bfd5a05cdf42fe598128c1a21261f3932d389
bhoon <leafloveu@naver.com>
2025-01-23 11:50:49 +0900

    feat(coordinate:triangle) - 삼각형 생성 검증 추가

--- src/main/java/coord2/domain/Triangle.java
+++ src/main/java/coord2/domain/Triangle.java
@@ -1,5 +1,7 @@
 package coord2.domain;
 
+import coord2.exception.IllegalTriangleException;
+
 public class Triangle implements Shape {
 
     private final Point[] points;
@@ -10,7 +12,13 @@ public class Triangle implements Shape {
     }
 
     private void validate(Point[] points) {
-
+        if(points.length != 3){
+            throw new IllegalTriangleException();
+        }
+        if(points[0].equals(points[1]) || points[1].equals(points[2])
+                || points[0].equals(points[2])){
+            throw new IllegalTriangleException();
+        }
     }
 
     @Override
